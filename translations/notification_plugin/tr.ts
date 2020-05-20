becb1b6be2cc2c9c0352958eddc234455b1abdd2
Aylinux <cihanalk@gmail.com>
2020-05-20 21:34:00 +0300

    Create tr.ts

---
+++ translations/notification_plugin/tr.ts
@@ -0,0 +1,83 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="tr">
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../../src/plugins/notification_plugin/notification_plugin.cpp" line="66"/>
+        <source>Notification center</source>
+        <translation type="unfinished">Bildirim Merkezi</translation>
+    </message>
+    <message>
+        <location filename="../../src/plugins/notification_plugin/notification_plugin.cpp" line="107"/>
+        <location filename="../../src/plugins/notification_plugin/notification_plugin.cpp" line="591"/>
+        <source>Important notice</source>
+        <translation type="unfinished">Önemli uyarı</translation>
+    </message>
+    <message>
+        <location filename="../../src/plugins/notification_plugin/notification_plugin.cpp" line="115"/>
+        <source>Clean up</source>
+        <translation type="unfinished">Temizlemek</translation>
+    </message>
+    <message>
+        <location filename="../../src/plugins/notification_plugin/notification_plugin.cpp" line="122"/>
+        <source>Set up</source>
+        <translation type="unfinished">Kur</translation>
+    </message>
+    <message>
+        <location filename="../../src/plugins/notification_plugin/notification_plugin.cpp" line="146"/>
+        <source>No new notifications</source>
+        <translation type="unfinished">Yeni bildirim yok</translation>
+    </message>
+    <message>
+        <location filename="../../src/plugins/notification_plugin/notification_plugin.cpp" line="169"/>
+        <source>No unimportant notice</source>
+        <translation type="unfinished">没有不重要的通知</translation>
+    </message>
+    <message>
+        <location filename="../../src/plugins/notification_plugin/notification_plugin.cpp" line="561"/>
+        <source>Unimportant notice</source>
+        <translation type="unfinished">不重要的通知</translation>
+    </message>
+</context>
+<context>
+    <name>SingleMsg</name>
+    <message>
+        <location filename="../../src/plugins/notification_plugin/singlemsg.cpp" line="117"/>
+        <source>now</source>
+        <translation type="unfinished">现在</translation>
+    </message>
+    <message>
+        <location filename="../../src/plugins/notification_plugin/singlemsg.cpp" line="281"/>
+        <source>Yesterday </source>
+        <translation type="unfinished">昨天 </translation>
+    </message>
+    <message>
+        <location filename="../../src/plugins/notification_plugin/singlemsg.cpp" line="360"/>
+        <source>In addition </source>
+        <translation type="unfinished">还有 </translation>
+    </message>
+    <message>
+        <location filename="../../src/plugins/notification_plugin/singlemsg.cpp" line="360"/>
+        <source> notification</source>
+        <translation type="unfinished"> 则通知</translation>
+    </message>
+    <message>
+        <source>In addition</source>
+        <translation type="obsolete">还有</translation>
+    </message>
+</context>
+<context>
+    <name>TakeInBoxToolButton</name>
+    <message>
+        <location filename="../../src/plugins/notification_plugin/takeinboxtoolbutton.cpp" line="36"/>
+        <source>Enter unimportant news</source>
+        <translation type="unfinished">进入不重要通知</translation>
+    </message>
+    <message>
+        <location filename="../../src/plugins/notification_plugin/takeinboxtoolbutton.cpp" line="41"/>
+        <source>Quit unimportant news</source>
+        <translation type="unfinished">退出不重要通知</translation>
+    </message>
+</context>
+</TS>
